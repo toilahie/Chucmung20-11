9f230bbaf2837841ca9a90d91959013316b6c93a
ToiLaHiep02 <92879998+toilahiep00@users.noreply.github.com>
2021-11-20 11:35:20 +0700

    update

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Cô Thùy';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc mừng 20/11';
+const titleCard = 'Tặng Cô';
+const contentCard = 'Nhân dịp 20/11, e chúc cô luôn mạnh khỏe, trẻ đẹp và thành công trong công việc giảng dạy, kèm cặp học sinh rất tốt cho cuộc sống sau này.';
+const giftImage = 'cogiao.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
